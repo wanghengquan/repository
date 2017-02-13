644b8ac5b8e6a890a1c936b6893188199693b4f4
Jason Wang <Jason.Wang@latticesemi.com>
2017-02-13 18:11:19 +0800

    project build

---
+++ tmp_client/src/info_parser/cmd_parser.java
@@ -0,0 +1,42 @@
+/*
+ * File: cmd_parser.java
+ * Description: Software command line parser definition
+ * Author: Jason.Wang
+ * Date:2017/02/13
+ * Modifier:
+ * Date:
+ * Description:
+ */
+package info_parser;
+
+
+import org.apache.commons.cli.*;
+import org.apache.logging.log4j.*;
+
+
+/*
+ * PlatUML graph
+ * @startuml
+ * :Hello world;
+ * :This is on defined on
+ * several **lines**;
+ * @enduml
+ */
+class cmd_parser {
+	//public property
+	//protect property
+	//private property
+	//public function
+	//protect function
+	//private function
+	public cmd_parser()
+	{
+	
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
