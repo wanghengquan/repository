97b13ca3f7a8ca73529fd9a585643c4dd52a7c7e
Jason.Wang <wanghengquan003@163.com>
2017-02-18 20:23:31 +0800

    add cpu and mem check for windows

---
+++ tmp_client/tools/get_cpu.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import psutil
+
+top_dir = os.getcwd()
+
+ON_WIN = sys.platform.startswith("win")
+if not ON_WIN:
+    sys.exit('0')
+cpu_usage = psutil.cpu_percent(interval=5)
+print int(cpu_usage)
---
+++ tmp_client/tools/get_mem.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import psutil
+
+top_dir = os.getcwd()
+
+ON_WIN = sys.platform.startswith("win")
+if not ON_WIN:
+    sys.exit('0')
+mem_tuple= psutil.virtual_memory()
+mem_usage = mem_tuple[2]
+print int(mem_usage)
