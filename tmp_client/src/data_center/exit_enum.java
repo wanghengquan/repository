83b0fa7c17b485faf5c999fa1660d8503b5d4298
Jason Wang <Jason.Wang@latticesemi.com>
2017-07-20 18:33:56 +0800

    update

--- tmp_client/src/data_center/exit_enum.java
+++ tmp_client/src/data_center/exit_enum.java
@@ -13,12 +13,12 @@ public enum exit_enum {
 	NORMAL(0, "Normal software eixt, all good."),
 	TASK(1, "Normal software exit with some failed task/case."),
 	DUMP(2, "Software core dump."),
-	UPDATE(2, "Software self-update exit."),
-	FLOW(3, "Software engine flow exit."),
-	DATA(4, "Software engine data server exit."),
-	TUBE(5, "Connect tube exit."),
-	GUI(6, "Software GUI exit."),
-	OTHERS(7, "Others/Unknown.");
+	UPDATE(3, "Software self-update exit."),
+	FLOW(4, "Software engine flow exit."),
+	DATA(5, "Software engine data server exit."),
+	TUBE(6, "Connect tube exit."),
+	GUI(7, "Software GUI exit."),
+	OTHERS(8, "Others/Unknown.");
 	private int index;
 	private String description;
 	
