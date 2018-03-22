95b38d3535079cf3cd296df802434a1b9546a565
Jason Wang <Jason.Wang@latticesemi.com>
2018-03-22 18:04:20 +0800

    pscp bug fix

--- tmp_client/src/flow_control/task_prepare.java
+++ tmp_client/src/flow_control/task_prepare.java
@@ -247,7 +247,7 @@ public class task_prepare {
 						// pscp -r -p -l jwang1 -pw lattice1
 						// lsh-comedy:/public/jason_test/temp/src
 						// c:/users\jwang1\Desktop
-						cmd_array.add(public_data.TOOLS_PSCP + " -r -p -l -batch" + user_name + " -pw " + pass_word + " "
+						cmd_array.add(public_data.TOOLS_PSCP + " -r -p -batch -l " + user_name + " -pw " + pass_word + " "
 								+ case_url + " " + case_dir);
 					} else {
 						// 2 windows run windows src
