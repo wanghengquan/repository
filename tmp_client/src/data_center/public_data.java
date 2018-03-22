2454c31865e26f7056bf00bfaba7e74ef1feb7e5
Jason Wang <Jason.Wang@latticesemi.com>
2018-03-22 18:07:00 +0800

    pscp bug fix

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -24,9 +24,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.07.02"; //2.06.12 External.Internal.DEV
-	public final static int BASE_CURRENTVERSION_INT = 20702; // version for code use
-	public final static String BASE_BUILDDATE = "2018/03/05";
+	public final static String BASE_CURRENTVERSION = "2.07.03"; //2.06.12 External.Internal.DEV
+	public final static int BASE_CURRENTVERSION_INT = 20703; // version for code use
+	public final static String BASE_BUILDDATE = "2018/03/22";
 	public final static String BASE_SUITEFILEVERSION = "1.09";
 	public final static String BASE_CONTACT_MAIL = "Jason.Wang@latticesemi.com";
 	public final static float BASE_JAVABASEVERSION = 1.8f;
