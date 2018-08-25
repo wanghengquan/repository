61198001560f9e9b4066d622d52c361f9483eba2
Jason.Wang <wanghengquan003@163.com>
2018-08-25 12:10:41 +0800

    i126--Client mail function support

---
+++ tmp_client/src/utility_funcs/mail_action.java
@@ -0,0 +1,190 @@
+/*
+ * File: system_cmd.java
+ * Description: run system command line
+ * Author: Jason.Wang
+ * Date:2018/08/21
+ * Modifier:
+ * Date:
+ * Description:
+ */
+package utility_funcs;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.mail.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import data_center.public_data;
+import data_center.client_data;
+
+public class mail_action {
+	// public property
+	// protected property
+	// private property
+	private client_data client_info;
+	private static final Logger MAIL_ACTION_LOGGER = LogManager.getLogger(mail_action.class.getName());
+    private String mail_server = public_data.MAIL_SERVER;
+    private String def_operator = public_data.BASE_OPERATOR_MAIL;
+    private String def_deveoper = public_data.BASE_DEVELOPER_MAIL;
+    private String line_separator = System.getProperty("line.separator");
+    
+    		
+	public mail_action(client_data client_info) {
+		this.client_info = client_info;
+	}
+	
+	protected void simple_dump_mail(
+			String message
+			){
+		String machine = client_info.get_client_machine_data().get("terminal");
+		String to_str = client_info.get_client_preference_data().get("dev_mails");
+		String cc_str = client_info.get_client_preference_data().get("opr_mails");
+		StringBuilder send_massage = new StringBuilder("");
+		String current_time = time_info.get_date_time();
+		send_massage.append("Time:" + current_time);
+		send_massage.append(line_separator);
+		send_massage.append("Machine:" + machine);
+		send_massage.append(line_separator);
+		send_massage.append("Dump info:");
+		send_massage.append(line_separator);		
+		send_massage.append(message);
+		try {
+			Email simple_mail = new SimpleEmail();
+			simple_mail.setHostName(mail_server);
+			simple_mail.setFrom(def_operator);
+			simple_mail.setTo(prepare_send_list(to_str));			
+			simple_mail.setSubject("TMP client dumpped.");
+			simple_mail.setCc(prepare_send_list(cc_str));
+			simple_mail.addBcc(def_deveoper);			
+			simple_mail.setMsg(send_massage.toString());
+			simple_mail.send();
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			//  e.printStackTrace();
+			MAIL_ACTION_LOGGER.error("Send client dump message error out.");
+		}		
+	}
+	
+	
+	protected void simple_event_mail(
+			String events,
+			String message
+			){
+		String to_str = client_info.get_client_preference_data().get("opr_mails");
+		String send_massage = new String(message);
+		try {
+			Email simple_mail = new SimpleEmail();
+			simple_mail.setHostName(mail_server);
+			simple_mail.setFrom(def_operator);
+			simple_mail.setTo(prepare_send_list(to_str));			
+			simple_mail.setSubject("TMP client : " + events);			
+			simple_mail.setMsg(send_massage);
+			simple_mail.send();
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			//  e.printStackTrace();
+			MAIL_ACTION_LOGGER.error("Send client dump message error out.");
+		}			
+	}
+	
+	protected void multipart_attached_mail(
+			String events,
+			String file_path,
+			String message
+			){
+		String to_str = client_info.get_client_preference_data().get("opr_mails");
+		String send_url = new String(file_path);
+		File attach_file = new File(file_path);
+		if (!attach_file.exists()){
+			MAIL_ACTION_LOGGER.error("Attached file path not exists: " + file_path);
+			return;
+		}
+		if (attach_file.isDirectory()){
+			send_url = get_zipped_file(file_path);
+		}
+		File url_fobj = new File(send_url);
+		String attach_name = new String("NA");
+		if (url_fobj.exists()){
+			attach_name = url_fobj.getName();
+		}
+		EmailAttachment attachment = new EmailAttachment();
+		attachment.setPath(send_url);
+		attachment.setDisposition(EmailAttachment.ATTACHMENT);
+		attachment.setDescription("Attachment");
+		attachment.setName(attach_name);
+		try {
+			MultiPartEmail multipart_mail = new MultiPartEmail();
+			multipart_mail.setHostName(mail_server);
+			multipart_mail.setFrom(def_operator);
+			multipart_mail.setTo(prepare_send_list(to_str));
+			multipart_mail.setSubject("TMP client:" + events);
+			multipart_mail.setMsg(message);
+			multipart_mail.attach(attachment);
+			multipart_mail.send();
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+			MAIL_ACTION_LOGGER.error("Send client dump message error out.");
+		}		
+	}
+	
+	private String get_zipped_file(String dir_path){
+		String zip_file = new String("");
+		File dir_fobj = new File(dir_path);
+		String base_name = dir_fobj.getName();
+		String work_space = client_info.get_client_preference_data().get("work_space");
+		String zip_path = work_space + "/" + public_data.WORKSPACE_TEMP_DIR;
+		File zip_path_fobj = new File(zip_path);
+		if (!zip_path_fobj.exists()) {
+			try {
+				FileUtils.forceMkdir(zip_path_fobj);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				// e.printStackTrace();
+				MAIL_ACTION_LOGGER.error("Create temp zip path failed, skip zip file.");
+				return zip_file;
+			}
+		}
+		zip_file = zip_path + "/" + base_name + ".zip";
+		File zip_fobj = new File(zip_file);
+		if (zip_fobj.exists()){
+			FileUtils.deleteQuietly(zip_fobj);
+		}
+		file_action.zipFolder(dir_path.replaceAll("\\\\", "/"), zip_file);
+		return zip_file;		
+	}
+	
+	private ArrayList<InternetAddress> prepare_send_list(String to_string){
+		Pattern mail_patt = Pattern.compile("(\\w)+(.\\w+)*@(\\w)+((.\\w+)+)", Pattern.CASE_INSENSITIVE);
+		ArrayList<InternetAddress> mail_array = new ArrayList<InternetAddress>();
+		for (String mail_addr : to_string.split(",|;")) {
+			Matcher mail_match = mail_patt.matcher(mail_addr.trim());
+			if (mail_match.find()){
+				try {
+					mail_array.add(new InternetAddress(mail_addr.trim()));
+				} catch (AddressException e) {
+					// TODO Auto-generated catch block
+					// e.printStackTrace();
+					MAIL_ACTION_LOGGER.error("Wrong mail address parsed for: " + mail_addr);
+				}
+			}
+		}
+		return mail_array;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Please start from:top_runner/top_launcher");
+		client_data client_info = new client_data();
+		mail_action mail = new mail_action(client_info);
+		mail.multipart_attached_mail("events", "D:/tmp_work_space/results/prj9/runtotal_eit_suites_122717_141445/Tm1_1_", "testing...");
+	}
+}
