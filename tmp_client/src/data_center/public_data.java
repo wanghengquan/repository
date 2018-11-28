40c0bf7fbeab908eb71cea966ff5d9f343f4e18c
Jason.Wang <wanghengquan003@163.com>
2018-11-28 17:41:22 +0800

    i149--retest on 'No data...' case client will crash

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -24,9 +24,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.08.47"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 20847; // version for code use
-	public final static String BASE_BUILDDATE = "2018/11/23";
+	public final static String BASE_CURRENTVERSION = "2.08.48"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 20848; // version for code use
+	public final static String BASE_BUILDDATE = "2018/11/28";
 	public final static String BASE_SUITEFILEVERSION = "1.11";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
