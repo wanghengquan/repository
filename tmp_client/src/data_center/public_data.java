5fd65e82c304146233c28c3ff07a08a4a699a563
Jason.Wang <wanghengquan003@163.com>
2019-04-08 16:21:02 +0800

    i192--remove core script displayed in software

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -24,9 +24,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.08.91"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 20891; // version for code use
-	public final static String BASE_BUILDDATE = "2019/04/07";
+	public final static String BASE_CURRENTVERSION = "2.08.92"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 20892; // version for code use
+	public final static String BASE_BUILDDATE = "2019/04/08";
 	public final static String BASE_SUITEFILEVERSION = "1.12";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
