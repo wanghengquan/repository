80daf667d48a4127e6c76b4690f796e4ee1d9340
Jason.Wang <wanghengquan003@163.com>
2019-04-12 14:47:28 +0800

    i195--report client core script version to TMP

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -24,8 +24,8 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.08.94"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 20894; // version for code use
+	public final static String BASE_CURRENTVERSION = "2.08.95"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 20895; // version for code use
 	public final static String BASE_BUILDDATE = "2019/04/12";
 	public final static String BASE_SUITEFILEVERSION = "1.12";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
