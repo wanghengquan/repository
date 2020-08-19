97739024ba3b935b8f2f2a1c7e4b6fe8270a0c7e
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2020-08-19 16:37:07 +0800

    i291--upstream synplify error message @E

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -26,9 +26,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.10.93"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 21093; // version for code use
-	public final static String BASE_BUILDDATE = "2020/07/23";
+	public final static String BASE_CURRENTVERSION = "2.10.94"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 21094; // version for code use
+	public final static String BASE_BUILDDATE = "2020/08/19";
 	public final static String BASE_SUITEFILEVERSION = "1.16";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
