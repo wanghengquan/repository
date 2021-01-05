a1f80ae3a4067869afe9ce26d63bd435581ef725
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2021-01-05 16:20:54 +0800

    i313--local case sorting issue fix

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -26,9 +26,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.12.16"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 21216; //version for code use
-	public final static String BASE_BUILDDATE = "2020/12/28";
+	public final static String BASE_CURRENTVERSION = "2.12.17"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 21217; //version for code use
+	public final static String BASE_BUILDDATE = "2021/01/05";
 	public final static String BASE_SUITEFILEVERSION = "1.17";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
