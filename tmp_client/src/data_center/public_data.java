5bf572a633e80fedf913b5cfce504131ce142566
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2021-12-01 09:43:04 +0800

    i382--bug fix for empty admin data

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -26,9 +26,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.12.84"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 21284; //version for code use
-	public final static String BASE_BUILDDATE = "2021/11/30";
+	public final static String BASE_CURRENTVERSION = "2.12.85"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 21285; //version for code use
+	public final static String BASE_BUILDDATE = "2021/12/01";
 	public final static String BASE_SUITEFILEVERSION = "1.23";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
