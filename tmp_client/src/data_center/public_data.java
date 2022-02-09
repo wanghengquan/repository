f5d3e20eef8976db8a31597a485b54c7fd689e90
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2022-02-09 15:39:43 +0800

    i404--Update Apollo EIT2 suite file

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -27,9 +27,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.14.06"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 21406; //version for code use
-	public final static String BASE_BUILDDATE = "2022/02/08";
+	public final static String BASE_CURRENTVERSION = "2.14.07"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 21407; //version for code use
+	public final static String BASE_BUILDDATE = "2022/02/09";
 	public final static String BASE_SUITEFILEVERSION = "1.24";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
