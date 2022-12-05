fd2fe8d87be3284a6729380de1477a9d2eac8cd1
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2022-12-05 10:14:38 +0800

    dev version issue fix

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -28,7 +28,7 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.14.422"; //main.xx.build. xx:odd for stable, even for develop
+	public final static String BASE_CURRENTVERSION = "2.14.42"; //main.xx.build. xx:odd for stable, even for develop
 	public final static int BASE_CURRENTVERSION_INT = 21442; //version for code use
 	public final static String BASE_BUILDDATE = "2022/12/05";
 	public final static String BASE_SUITEFILEVERSION = "1.26";
