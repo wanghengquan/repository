05ae72787f90a8468540f5ddb39268a47e4d3d5f
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2023-07-12 15:38:47 +0800

    i465--suite file multi-match update

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -28,9 +28,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.14.67"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 21467; //version for code use
-	public final static String BASE_BUILDDATE = "2023/07/04";
+	public final static String BASE_CURRENTVERSION = "2.14.68"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 21468; //version for code use
+	public final static String BASE_BUILDDATE = "2023/07/12";
 	public final static String BASE_SUITEFILEVERSION = "1.28";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
