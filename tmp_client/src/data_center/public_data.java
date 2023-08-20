d34b091ff19e16741fc1e9cd903a90df10229aac
jwang1lattice <69793347+jwang1lattice@users.noreply.github.com>
2023-08-20 20:54:27 +0800

    i470--sw name with multiple dot support

--- tmp_client/src/data_center/public_data.java
+++ tmp_client/src/data_center/public_data.java
@@ -28,9 +28,9 @@ public class public_data {
 	// ========================
 	// base 
 	// end with 0: long term version, otherwise developing version
-	public final static String BASE_CURRENTVERSION = "2.14.72"; //main.xx.build. xx:odd for stable, even for develop
-	public final static int BASE_CURRENTVERSION_INT = 21472; //version for code use
-	public final static String BASE_BUILDDATE = "2023/07/28";
+	public final static String BASE_CURRENTVERSION = "2.14.73"; //main.xx.build. xx:odd for stable, even for develop
+	public final static int BASE_CURRENTVERSION_INT = 21473; //version for code use
+	public final static String BASE_BUILDDATE = "2023/08/20";
 	public final static String BASE_SUITEFILEVERSION = "1.28";
 	public final static String BASE_DEVELOPER_MAIL = "Jason.Wang@latticesemi.com";
 	public final static String BASE_OPERATOR_MAIL = "Jason.Wang@latticesemi.com";
